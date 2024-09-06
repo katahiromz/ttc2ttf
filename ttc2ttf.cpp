2cc48bdc5634d4780a99265428110e5a1f6442c3
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-09-06 17:32:26 +0900

    improve

--- ttc2ttf.cpp
+++ ttc2ttf.cpp
@@ -67,11 +67,25 @@ static inline uint16_t u16_endian_fix(uint16_t value)
     return u16_swab(value);
 }
 
-template <typename T_TYPE>
-static inline uint32_t u32_get(const T_TYPE& input, size_t offset)
+template <typename T_INPUT>
+static inline uint16_t u16_get(const T_INPUT& input, size_t offset)
+{
+    input.at(offset + sizeof(uint16_t) - 1);
+    return u16_endian_fix(*reinterpret_cast<const uint16_t *>(&input.at(offset)));
+}
+
+template <typename T_INPUT>
+static inline uint32_t u32_get(const T_INPUT& input, size_t offset)
 {
     input.at(offset + sizeof(uint32_t) - 1);
     return u32_endian_fix(*reinterpret_cast<const uint32_t *>(&input.at(offset)));
+}
+
+template <typename T_OUTPUT>
+static inline void u32_set(T_OUTPUT& output, size_t offset, uint32_t value)
+{
+    output.at(offset + sizeof(uint32_t) - 1);
+    *reinterpret_cast<uint32_t *>(&output.at(offset)) = u32_endian_fix(value);
 }
 
 bool file_read_all(const tstring& filename, std::vector<char>& content)
@@ -110,7 +124,7 @@ void ttc2ttf_extract(std::vector<char>& output, const std::vector<char>& input, 
     _tprintf(_T("Extract TTF #%d\n"), index);
     _tprintf(_T("\tHeader offset %d\n"), ttf_offset);
 
-    uint16_t table_count = u16_endian_fix(*reinterpret_cast<const uint16_t*>(&input.at(ttf_offset + 4)));
+    uint16_t table_count = u16_get(input, ttf_offset + 4);
     uint32_t header_length = 12 + table_count * 16;
     _tprintf(_T("\tHeader size %d bytes\n"), header_length);
 
@@ -130,7 +144,7 @@ void ttc2ttf_extract(std::vector<char>& output, const std::vector<char>& input, 
     {
         uint32_t offset = u32_get(input, ttf_offset + 12 + 8 + j * 16);
         uint32_t length = u32_get(input, ttf_offset + 12 + 12 + j * 16);
-        *reinterpret_cast<uint32_t*>(&output.at(12 + 8 + j * 16)) = u32_endian_fix(current_offset);
+        u32_set(output, 12 + 8 + j * 16, current_offset);
         std::memcpy(&output.at(current_offset), &input.at(offset), length);
         current_offset += u32_ceil4(length);
     }
@@ -143,7 +157,7 @@ int ttc2ttf_get_ttf_count(const std::vector<char>& input)
         return -1;
 
     // get the count of fonts
-    return (int)u32_endian_fix(*reinterpret_cast<const uint32_t*>(&input.at(8)));
+    return (int)u32_get(input, 8);
 }
 
 TTC2TTF_RET ttc2ttf_data_from_data(std::vector<char>& output, const std::vector<char>& input, int font_index)
